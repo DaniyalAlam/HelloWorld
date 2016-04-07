78d29f69f264fc2bbca181cfc7e657d03d00a3b1
Daniyal Alam <daniyalalam.95@gmail.com>
2016-04-07 19:18:33 -0400

    first commit

---
+++ HelloWorldForGit/src/com/daniyal/HelloWorldClass/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.daniyal.HelloWorldClass;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, this is just to test my github...");
+
+	}
+
+}
